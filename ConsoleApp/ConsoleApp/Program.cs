a597f05e01779132f686f78f50629ee16f4c69a0
abir <freewill.forcewill@gmail.com>
2019-11-07 23:38:40 +0600

    4

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp
 
             while (year < 1971 || year > 2019)
             {
-                Console.WriteLine("input correctly");
+                Console.WriteLine("sorry, you provide incorrect month");
                 year = int.Parse(Console.ReadLine());
             }
 
@@ -24,7 +24,7 @@ namespace ConsoleApp
 
             while (month < 1 || month > 12)
             {
-                Console.WriteLine("input correctly");
+                Console.WriteLine("sorry, you provide incorrect month");
                 month = int.Parse(Console.ReadLine());
             }
 
@@ -33,12 +33,82 @@ namespace ConsoleApp
 
             while (day < 1 || day > 31)
             {
-                Console.WriteLine("input correctly");
+                Console.WriteLine("sorry, you provide incorrect day");
                 day = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("your date is  " + year + "/" + month+"/" + day);
-            Console.ReadKey();
+            if (month == 2)
+            {
+                while (day > 28 || day < 1)
+                {
+                    Console.WriteLine("sorry, you provide incorrect day");
+                    day = int.Parse(Console.ReadLine());
+                };
+            }
+
+            if (month == 4 || month == 6 || month == 9 || month == 11 )
+            {
+                while (day > 30 || day < 1)
+                {
+                    Console.WriteLine("sorry, you provide incorrect day");
+                    day = int.Parse(Console.ReadLine());
+                };
+            }
+
+            else
+            {
+                while (day > 31 || day < 0)
+                {
+                    Console.WriteLine("sorry, you provide incorrect day");
+                    day = int.Parse(Console.ReadLine());
+                };
+            }
+
+            Convert.ToString(month);
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("your date is  " + year + "/" + "jan" + "/" + day); ;
+                    break;
+                case 2:
+                    Console.WriteLine("your date is  " + year + "/" + "feb" + "/" + day); 
+                    break;
+                case 3:
+                    Console.WriteLine("your date is  " + year + "/" + "mar" + "/" + day); 
+                    break;
+                case 4:
+                    Console.WriteLine("your date is  " + year + "/" + "apr" + "/" + day); ;
+                    break;
+                case 5:
+                    Console.WriteLine("your date is  " + year + "/" + "may" + "/" + day); ;
+                    break;
+                case 6:
+                    Console.WriteLine("your date is  " + year + "/" + "jun" + "/" + day); ;
+                    break;
+                case 7:
+                    Console.WriteLine("your date is  " + year + "/" + "jul" + "/" + day); ;
+                    break;
+                case 8:
+                    Console.WriteLine("your date is  " + year + "/" + "aug" + "/" + day); ;
+                    break;
+                case 9:
+                    Console.WriteLine("your date is  " + year + "/" + "sep" + "/" + day); ;
+                    break;
+                case 10:
+                    Console.WriteLine("your date is  " + year + "/" + "oct" + "/" + day); ;
+                    break;
+                case 11:
+                    Console.WriteLine("your date is  " + year + "/" + "nov" + "/" + day); ;
+                    break;
+                case 12:
+                    Console.WriteLine("your date is  " + year + "/" + "dec" + "/" + day); ;
+                    break;
+                default:
+                    Console.WriteLine("input incorrectly") ;
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
