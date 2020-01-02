65f1878548eefb262c1df096dfd8e1dab8d2f08b
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-01-02 23:00:16 +0600

    h

---
+++ E-commerce/E-commerce/IEco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce
+{
+    interface IEco
+    {
+        void AddProduct();
+        void BuyProduct();
+        void Display();
+        void DisplayCustomersProduct();
+    }
+}
---
+++ E-commerce/E-commerce/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_commerce
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+
+        public Product(string name,int stock,int price)
+        {
+            Name = name;
+            Stock = stock;
+            Price = price;
+        }
+        public void AddStock(int quantity)
+        {
+            Stock += quantity;
+        }
+        public void SoldStock(int quantity)
+        {
+            Stock -= quantity;
+        }
+    }
+}
