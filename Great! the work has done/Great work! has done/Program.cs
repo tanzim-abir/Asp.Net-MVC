2663ee334f79c4a9e7dcd8fb90d5fcb72fda4996
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-02-22 11:37:30 +0600

    C# Code Practice

---
+++ Great! the work has done/Great work! has done/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Great_work__has_done
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total hours to finish the sprint");
+            int sprintHour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Total team member");
+            int totalMember = Convert.ToInt32(Console.ReadLine());
+
+            if (sprintHour <= 0 || sprintHour > 100 || totalMember <= 0 || totalMember > 100)
+            {
+                Console.WriteLine("wrong input");
+            }
+            else
+            {
+                while(totalMember!=0)
+            }
+        }
+    }
+}
