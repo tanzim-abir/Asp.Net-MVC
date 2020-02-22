50c296b675a1d33326f21cd9d8e9b4fd299e8cbc
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-02-22 17:13:02 +0600

    webapp

---
+++ Assingment 2. net web mvc/CompanyName.ProjectName.WebCoreApp/CompanyName.ProjectName.WebCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompanyName.ProjectName.WebCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
