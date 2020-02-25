99aa26d182691d7b0faf97ee32bbf0db2926426f
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-02-25 23:48:54 +0600

    1

---
+++ problem-solving/vely easy/velyeasy/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace velyeasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.Clear();
+            }
+            if (input < 1 && input > 20)
+            {
+                Console.WriteLine("wrond input");
+            }
+            else
+            {
+                while (input != 0)
+                {
+                    string s = Console.ReadLine();
+                    var regex = new Regex("^[a-z]+$");
+
+                    if (s.Length > 50 || !regex.IsMatch(s))
+
+                    {
+                        Console.WriteLine("char limit exceed or capital letter found");
+                    }
+                    else
+                    {
+
+                        if (s.Contains('r'))
+                        {
+                            Console.WriteLine("not vely easy");
+                        }
+                        else
+                        {
+                            Console.WriteLine("vely easy");
+                        }
+                    }
+
+                    input--;
+                }
+
+            }
+        }
+    }
+}
+
