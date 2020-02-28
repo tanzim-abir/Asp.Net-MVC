2d04d794bb87ba4c3942e3f505472bbd696a7a00
tanzim abir <55651997+tanzim-abir@users.noreply.github.com>
2020-02-28 07:46:56 +0600

    1

---
+++ Assignment3/CompanyName.ProjectName.WebCoreApp/CompanyName.ProjectName.WebCoreApp/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyName.ProjectName.WebCoreApp.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
